c4dc77278ba8acd9e3e18e34e6d628aa3c2b390d
Robin <robinlee_6316@msn.com>
2015-02-11 09:54:25 +0800

    針對多對多串連的查詢的範例

---
+++ MasterLinqDetail/DemoDataService.cs
@@ -0,0 +1,66 @@
+﻿using MasterLinqDetail.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MasterLinqDetail
+{
+    public class DemoDataService
+    {
+        #region Fields
+        private DemoDBEntities db = new DemoDBEntities();
+        #endregion
+
+        #region 針對單一個 Material，找出已選取的 MSHAPE
+        public List<MSHAPE> GetShapesWithLazyLoading(int materialId)
+        {
+            db.Configuration.ProxyCreationEnabled = true;
+
+            var collection = db.MATERIAL.Find(materialId).MSHAPE;
+            return new List<MSHAPE>(collection);
+        }
+
+        public List<MSHAPE> GetShapesWithoutLazyLoading(int materialId)
+        {
+            db.Configuration.ProxyCreationEnabled = false;
+
+            var collection = db.MATERIAL.Include("MSHAPE")
+                .FirstOrDefault(r=>r.ID==materialId).MSHAPE;
+
+            return new List<MSHAPE>(collection);
+        }
+        #endregion
+
+        #region 針對單一個 Material，找出尚未選取的 MSHAPE
+        public List<MSHAPE> GetUnselectedShapesWithLazyLoading(int materialId)
+        {
+            db.Configuration.ProxyCreationEnabled = true;
+
+            var result = (from q in db.MSHAPE
+                          where !(
+                          db.MATERIAL.Where(m => m.ID.Equals(materialId)).FirstOrDefault()
+                          .MSHAPE.Any(s => s.ID == q.ID)
+                          )
+                          select q
+                  );
+            return new List<MSHAPE>(result);
+        }
+
+        public List<MSHAPE> GetUnselectedShapesWithoutLazyLoading(int materialId)
+        {
+            db.Configuration.ProxyCreationEnabled = false;
+
+            var result = (from q in db.MSHAPE
+                          where !(
+                          db.MATERIAL.Where(m => m.ID.Equals(materialId)).FirstOrDefault()
+                          .MSHAPE.Any(s => s.ID == q.ID)
+                          )
+                          select q
+                  );
+            return new List<MSHAPE>(result);
+        }
+        #endregion
+    }
+}
---
+++ MasterLinqDetailTests/DemoDataServiceTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MasterLinqDetail;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MasterLinqDetail.Entities;
+namespace MasterLinqDetail.Tests
+{
+    [TestClass()]
+    public class DemoDataServiceTests
+    {
+        [TestMethod()]
+        public void GetShapesTest()
+        {
+            DemoDataService service = new DemoDataService();
+            List<MSHAPE> result = service.GetShapesWithLazyLoading(materialId: 1);
+            Assert.IsTrue(result.Count == 8);
+        }
+
+        [TestMethod()]
+        public void GetShapesWithoutLazyLoadingTest()
+        {
+            DemoDataService service = new DemoDataService();
+            List<MSHAPE> result = service.GetShapesWithoutLazyLoading(materialId: 1);
+            Assert.IsTrue(result.Count == 8);
+        }
+
+        [TestMethod()]
+        public void GetUnselectedShapesWithLazyLoadingTest()
+        {
+            DemoDataService service = new DemoDataService();
+            List<MSHAPE> result = service.GetUnselectedShapesWithLazyLoading(materialId: 1);
+            Assert.IsTrue(result.Count == 3);
+        }
+
+        [TestMethod()]
+        public void GetUnselectedShapesWithoutLazyLoadingTest()
+        {
+            DemoDataService service = new DemoDataService();
+            List<MSHAPE> result = service.GetUnselectedShapesWithoutLazyLoading(materialId: 1);
+            Assert.IsTrue(result.Count == 3);
+        }
+    }
+}
